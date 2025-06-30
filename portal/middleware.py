515209fb032f4b705b71c1a5748e8ebcb185a528
elvinsavio <elvinsavio.lp@gmail.com>
2025-06-30 10:22:26 +0000

    fix: redirect after login

--- portal/middleware.py
+++ portal/middleware.py
@@ -14,18 +14,14 @@ class AuthTokenMiddleware:
             return self.get_response(request)
 
         token = request.COOKIES.get('token')
-        print(f"Token: {token}")  # Debugging line to check the token value
         if not token:
             return redirect(reverse('login'))
         try:
             payload = jwt.decode(token, settings.JWT_SECRET, algorithms=["HS256"])
             request.user_payload = payload
-            print(f"Payload: {payload}")  # Debugging line to check the payload
         except jwt.ExpiredSignatureError:
-            print("Token has expired")
             return redirect('login')
         except jwt.InvalidTokenError:
-            print("Invalid token")
             return redirect('login')
         
         return self.get_response(request)
--- portal/views.py
+++ portal/views.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.contrib.auth import authenticate
 from django.http import HttpResponse
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from datetime import datetime, timedelta
 
 import jwt
@@ -13,27 +13,26 @@ def login(request):
         username = request.POST.get('username')
         password = request.POST.get('password')
         user = authenticate(request, username=username, password=password)
-        print(f"User: {user}, Username: {username}, Password: {password}")
         if user is not None:
             encoded_jwt = jwt.encode(
                 {'username': username}, 
                 settings.JWT_SECRET, 
                 algorithm='HS256'
             )
-
-            response = HttpResponse(redirect('index'))
+            response = HttpResponse()
+            response['HX-Redirect'] = '/'
             response.set_cookie(
                 'token', 
                 encoded_jwt, 
                 httponly=True, 
                 expires=datetime.today() + timedelta(days=1),
                 samesite='Lax',
-                secure=request.is_secure(),  # ensures compatibility
+                secure=request.is_secure(),
             )
             return response
         else:
             # Invalid credentials, show an error message
-            return render(request, 'login.html', {'error': 'Invalid credentials'})
+            return render(request, 'forms/login.html', {'error': 'Invalid credentials'})
       
 
     
